ae53d02d81fcba3224c152b5b4f8017999db06a6
Keith Lockwood <kwlockwo@gmail.com>
2022-02-25 18:16:21 +1100

    property config scanning

--- scheduler/src/main/java/net/afl/aflstats/SchedulerApplication.java
+++ scheduler/src/main/java/net/afl/aflstats/SchedulerApplication.java
@@ -2,8 +2,10 @@ package net.afl.aflstats;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class SchedulerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SchedulerApplication.class, args);
--- worker/src/main/java/net/afl/aflstats/WorkerApplication.java
+++ worker/src/main/java/net/afl/aflstats/WorkerApplication.java
@@ -2,10 +2,12 @@ package net.afl.aflstats;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableCaching
+@ConfigurationPropertiesScan
 public class WorkerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WorkerApplication.class, args);
